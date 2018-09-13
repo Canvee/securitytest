e4280c6a90192ad04ee96dc57efe718c985976d6
Kamil Czerwiński <klimacz@hotmail.com>
2018-09-13 09:29:43 +0200

    Added authenticationprovider

--- src/main/java/com/ZTI2018/securitytest/SecurityConfiguration.java
+++ src/main/java/com/ZTI2018/securitytest/SecurityConfiguration.java
@@ -1,13 +1,23 @@
 package com.ZTI2018.securitytest;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.ZTI2018.securitytest.security.CustomUserDetailsService;
+
 
 
 @Configuration
@@ -15,23 +25,61 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
+    private WebApplicationContext applicationContext;
+	private CustomUserDetailsService userDetailsService;
+	@Autowired
 	private DataSource dataSource;
-
-	@Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-		.usersByUsernameQuery("select username,password,enabled from users where username = ?")
-		.authoritiesByUsernameQuery("select username,authority from authorities where username = ?");
-//        auth.inMemoryAuthentication()
-//                .withUser("ajay").password("{noop}test").roles("USER").and()
-//                .withUser("demo").password("{noop}test2").roles("ADMIN");
+	
+	
+	@PostConstruct
+    public void completeSetup() {
+        userDetailsService = applicationContext.getBean(CustomUserDetailsService.class);
+    }
+	
+//	@Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.jdbcAuthentication().dataSource(dataSource)
+//		.usersByUsernameQuery("select username,password,enabled from users where username = ?")
+//		.authoritiesByUsernameQuery("select username,authority from authorities where username = ?");
+////        auth.inMemoryAuthentication()
+////                .withUser("ajay").password("{noop}test").roles("USER").and()
+////                .withUser("demo").password("{noop}test2").roles("ADMIN");
+//    }
+	
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+            .passwordEncoder(encoder())
+            .and()
+            .authenticationProvider(authenticationProvider())
+            .jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select username,password,enabled from users where username = ?")
+    		.authoritiesByUsernameQuery("select username,authority from authorities where username = ?");
     }
 	
 	@Override
 	public void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
+		//httpSecurity.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
+		httpSecurity.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 		httpSecurity.csrf().disable();
 	}
 	
-	
+	  @Bean
+	    public DaoAuthenticationProvider authenticationProvider() {
+	        final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+	        authProvider.setUserDetailsService(userDetailsService);
+	        authProvider.setPasswordEncoder(encoder());
+	        return authProvider;
+	    }
+
+	    @Bean
+	    public PasswordEncoder encoder() {
+	        return new BCryptPasswordEncoder(11);
+	    }
+	    
+	    @Bean
+	    public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
+	        return new SecurityEvaluationContextExtension();
+	    }
 }
